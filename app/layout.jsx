431076ce95882e694bc15748c665cb96cb3e6739
capxmaseve <roekty@gmail.com>
2026-09-07 22:20:09 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  metadataBase: new URL("https://svas.kr"),
+  metadataBase: new URL("https://www.svas.kr"),
 
   title: "KOMEL | SVAS",
 
@@ -10,22 +10,20 @@ export const metadata = {
     "SVAS가 개발 중인 휴대형 리필 퍼스널 향 오브제 KOMEL. 현재 기구설계 및 워킹 시제품 개발을 진행하고 있습니다.",
 
   alternates: {
-    canonical: "https://svas.kr",
+    canonical: "https://www.svas.kr",
   },
 
   openGraph: {
     title: "KOMEL | SVAS",
     description:
       "아이디어에서 실제 제품으로. SVAS가 개발 중인 퍼스널 향 오브제 KOMEL.",
-    url: "https://svas.kr",
+    url: "https://www.svas.kr",
     siteName: "SVAS",
     locale: "ko_KR",
     type: "website",
-
     images: [
       {
-        url: "https://svas.kr/og-svas.png",
-        secureUrl: "https://svas.kr/og-svas.png",
+        url: "https://www.svas.kr/og-svas.png",
         width: 1200,
         height: 630,
         alt: "SVAS",
@@ -39,19 +37,11 @@ export const metadata = {
     title: "KOMEL | SVAS",
     description:
       "아이디어에서 실제 제품으로. SVAS가 개발 중인 퍼스널 향 오브제 KOMEL.",
-    images: ["https://svas.kr/og-svas.png"],
+    images: ["https://www.svas.kr/og-svas.png"],
   },
 
   icons: {
-    icon: [
-      {
-        url: "/favicon.ico",
-      },
-      {
-        url: "/icon.png",
-        type: "image/png",
-      },
-    ],
+    icon: "/favicon.ico",
     apple: "/apple-icon.png",
   },
 
